1d9499a095de7a37e9b90c53fee5173dfaa55464
jxunhappy <lookto2008@gmail.com>
2017-11-20 09:47:47 +0800

    format code.

---
+++ py/format.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+
+'''
+format c/cpp files in directory
+format.py file1 file2 dir1 dir2
+clang-format must be installed.
+'''
+
+import subprocess
+import os
+import sys
+
+def format(file):
+    args = ['clang-format', '-style=file', '-i', file]
+    subprocess.call(args)
+
+if __name__ == '__main__':
+    for arg in sys.argv[1:]:
+        if os.path.isfile(arg):
+            format(sys.argv[1])
+        elif os.path.isdir(arg):
+            for folder, _, files in os.walk(arg):
+                for src_file in files:
+                    src_file = os.path.join(folder, src_file)
+                    if src_file.endswith('.h') or src_file.endswith('.cpp') or src_file.endswith('.cc'):
+                        print('format file: ' + src_file)
+                        format(src_file)
+        else:
+            print("skip: " + arg)    
+
